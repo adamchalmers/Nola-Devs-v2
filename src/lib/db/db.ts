2dcef6d08672e13d7992aebdf583401396c28a0f
Jorge <jorge@codingMustache.dev>
2024-03-30 23:45:56 -0500

    returning client from connection to DB
    Co-authored-by: Colin <colin.williams.dev@gmail.com>
    Co-authored-by: Thomas <git@Aeshus.com>

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,17 +1,14 @@
-import { connect } from 'mongoose';
+import { connect, createConnection } from 'mongoose';
+
 import { MONGO_URL, DB_NAME } from '$env/static/private';
 
 // setting up the connection to the DB
 const connectDB = async () => {
 	const dbUri = `${MONGO_URL}${DB_NAME}`;
-
-	// if (connection.readyState === 1) {
-	//     console.log('Already connected to the database');
-	//     return;
-	// }
 	try {
-		await connect(dbUri);
-		console.log('Database connected successfully');
+		connect(dbUri)
+		const conn = await createConnection(dbUri).asPromise();
+		return conn.getClient();
 	} catch (error) {
 		console.error('Database connection failed:', error);
 		process.exit(1);
