5643c23ff39ff56104d03407ca888a4aed42e150
Jorge <jorge@codingMustache.dev>
2024-03-30 23:47:24 -0500

    removed auth
    Co-authored-by: Colin <colin.williams.dev@gmail.com>
    Co-authored-by: Thomas <git@Aeshus.com>

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -3,25 +3,27 @@ import UserModel from '$lib/db/users';
 import { redirect } from '@sveltejs/kit';
 import bcrypt from 'bcrypt';
 import { randomBytes } from 'node:crypto';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+
 
 import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ cookies }) => {
-	const browserSes = cookies.get('session');
-	const dbSes = await SessionModel.findOne({ id: browserSes });
-
-	if (browserSes === dbSes?.id && dbSes) {
-		const user = await UserModel.findById(dbSes?.user);
-		if (Number(dbSes?.expire) >= Date.now() + 1000 * 60 * 60 * 24 * 30) {
-			await SessionModel.deleteOne({ id: browserSes });
-			cookies.delete('session');
-		} else {
-			throw redirect(302, `/admin/${user?.role}`);
-		}
-	} else {
-		cookies.delete('session');
-		return { success: false }; // wrong password or email
-	}
+	// const browserSes = cookies.get('session');
+	// const dbSes = await SessionModel.findOne({ id: browserSes });
+	
+	// if (browserSes === dbSes?.id && dbSes) {
+	// 	const user = await UserModel.findById(dbSes?.user);
+	// 	if (Number(dbSes?.expire) >= Date.now() + 1000 * 60 * 60 * 24 * 30) {
+	// 		await SessionModel.deleteOne({ id: browserSes });
+	// 		cookies.delete('session');
+	// 	} else {
+	// 		throw redirect(302, `/admin/${user?.role}`);
+	// 	}
+	// } else {
+	// 	cookies.delete('session');
+	// 	return { success: false }; // wrong password or email
+	// }
 };
 
 export const actions: Actions = {
@@ -54,7 +56,7 @@ export const actions: Actions = {
 				throw redirect(302, `/admin/${userpw.role}`); // correct password and email
 			}
 		}
-		setTimeout(() => {}, 1000); // Prolongs bruteforce attacks
+		setTimeout(() => { }, 1000); // Prolongs bruteforce attacks
 		return { success: false }; // wrong password or email
 	}
 };
